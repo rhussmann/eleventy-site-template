1a656b607508b0c040883ba97825a12161659e6b
Ricky Hussmann <ricky.hussmann@gmail.com>
2019-03-02 22:17:28 -0500

    Format infrastructure script files a bit better
    * Break long commands across lines

--- scripts/createStack.sh
+++ scripts/createStack.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
 STACK_NAME=$1
-DOMAIN_NAME=`jq -r '.[] | select(.ParameterKey == "DomainName") | .ParameterValue' < cloudformation/parameters.json`
+DOMAIN_NAME=`jq -r '.[] \
+    | select(.ParameterKey == "DomainName") \
+    | .ParameterValue' < cloudformation/parameters.json`
 
 echo "Creating stack $STACK_NAME for domain $DOMAIN_NAME..."
 
@@ -11,7 +13,8 @@ STACK_ID=`aws cloudformation create-stack \
     --parameters=file://cloudformation/parameters.json \
     | jq -r '.StackId'`
 
-while [ `aws cloudformation describe-stacks --stack-name=$STACK_ID | jq -r '.Stacks[0].StackStatus'` != "CREATE_COMPLETE" ]; do
+while [ `aws cloudformation describe-stacks --stack-name=$STACK_ID | \
+            jq -r '.Stacks[0].StackStatus'` != "CREATE_COMPLETE" ]; do
     echo "Waiting for stack creation completion..."
     sleep 3
 done
--- scripts/deleteStack.sh
+++ scripts/deleteStack.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env sh
 
 STACK_NAME=$1
-DOMAIN_NAME=`jq -r '.[] | select(.ParameterKey == "DomainName") | .ParameterValue' < cloudformation/parameters.json`
+DOMAIN_NAME=`jq -r '.[] \
+    | select(.ParameterKey == "DomainName") \
+    | .ParameterValue' < cloudformation/parameters.json`
 
 echo "Deleting stack $STACK_NAME for domain $DOMAIN_NAME..."
 
-STACK_ID=`aws cloudformation list-stacks | jq -r '.StackSummaries[] | select(.StackName == "eleventy-site-template" and .StackStatus == "CREATE_COMPLETE") | .StackId'`
+STACK_ID=`aws cloudformation list-stacks \
+    | jq -r '.StackSummaries[] \
+    | select(.StackName == "eleventy-site-template" and .StackStatus == "CREATE_COMPLETE") | .StackId'`
 
 aws s3 rb --force s3://$DOMAIN_NAME
 aws cloudformation delete-stack --stack-name $STACK_ID
 
-while [ `aws cloudformation describe-stacks --stack-name=$STACK_ID | jq -r '.Stacks[0].StackStatus'` != "DELETE_COMPLETE" ]; do
+while [ `aws cloudformation describe-stacks --stack-name=$STACK_ID \
+            | jq -r '.Stacks[0].StackStatus'` != "DELETE_COMPLETE" ]; do
     echo "Waiting for stack deletion completion..."
     sleep 3
 done
